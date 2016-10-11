a1bb9cc4cf889008080da09711be4f2d9856933f
Igor Palaguta <igor.palaguta@gmail.com>
2016-10-11 12:14:35 +0300

    Add Carthage Support

---
+++ Commander.h
@@ -0,0 +1,19 @@
+//
+//  Commander.h
+//  Commander
+//
+//  Created by Igor Palaguta on 11.10.16.
+//  Copyright © 2016 Igor Palaguta. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Commander.
+FOUNDATION_EXPORT double CommanderVersionNumber;
+
+//! Project version string for Commander.
+FOUNDATION_EXPORT const unsigned char CommanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commander/PublicHeader.h>
+
+
